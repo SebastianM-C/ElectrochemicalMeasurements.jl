fe9e54067bd8a3433df2b8aa1b645abde3d1f7ee
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2022-10-04 05:54:18 +0300

    Add GCD

---
+++ src/gcd.jl
@@ -0,0 +1,16 @@
+struct GCDMeasurement{D} <: AbstractMeasurement
+    dataset::D
+end
+
+default_select(gcd::GCDMeasurement) = [gcd.potential, gcd.time]
+
+@recipe function f(gcd::GCDMeasurement)
+    df = open(gcd)
+    V = gcd.poten
+    t = gcd.time
+    @series begin
+        xlabel --> t
+        ylabel --> V
+        df[!, t], df[!, V]
+    end
+end
