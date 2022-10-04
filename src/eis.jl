54b3db27f250d3b3b19a6ac78cd57c845e893e8e
Sebastian Micluța-Câmpeanu <m.c.sebastian95@gmail.com>
2022-10-04 19:07:58 +0300

    Improve EIS plots

--- src/eis.jl
+++ src/eis.jl
@@ -15,9 +15,14 @@ end
     df = open(eis)
     z1 = eis."Z1_name"
     z2 = eis."Z2_name"
+    Z_max = max(maximum(df[!, z1]), maximum(df[!, z2]))
     @series begin
+        seriestype --> :scatter
         xlabel --> z1
         ylabel --> z2
+        aspect_ratio := 1
+        xlims --> (0, Z_max*1.05)
+        ylims --> (0, Z_max*1.05)
         df[!, z1], df[!, z2]
     end
 end
@@ -32,8 +37,10 @@ end
     ϕ = eis."phase"
     f = eis."freq"
     @series begin
-        xlabel --> ϕ
-        ylabel --> f
-        df[!, ϕ], df[!, f]
+        seriestype --> :scatter
+        xlabel --> f
+        ylabel --> ϕ
+        xscale --> :log10
+        df[!, f], df[!, ϕ]
     end
 end
